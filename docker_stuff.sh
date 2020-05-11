158918c954bb55eb41414f83302bf6b01a8c88a1
Sean Ellis <sellis@twitter.com>
2020-05-11 10:29:43 -0700

    Make container interactive

--- docker_stuff.sh
+++ docker_stuff.sh
@@ -2,7 +2,7 @@
 
 function main() {
   docker build -t py3slim .
-  docker run --rm -v $(pwd):/treborsux -t py3slim /bin/bash
+  docker run --rm -v $(pwd):/treborsux -it py3slim /bin/bash
   # pip install -e .
 }
 
