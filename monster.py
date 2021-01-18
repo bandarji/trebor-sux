5fdd65b0e9d11926db14ef3dcfaab220f4082d09
Sean Jain Ellis <sellis@bandarji.com>
2021-01-18 08:31:24 -0800

    Enum and data classes added

--- monster.py
+++ monster.py
@@ -6,9 +6,39 @@ for m in monsters:
     print(m)
     print(m.spells)
 
-me = Adventurer(name='LEGOLAS', race='elf', new=True)
+
+
+
+# me = Adventurer(name='LEGOLAS', race='elf', new=True)
+# print(me)
+# me = Adventurer(name='GIMLI', race='dwarf', new=True)
+# print(me)
+# stats = {
+#     'strength': 17,
+#     'intelligence': 17,
+#     'wisdom': 17,
+#     'constitution': 17,
+#     'charisma': 17,
+#     'luck': 17,
+#     'dexterity': 17,
+# }
+# me = Adventurer(name='BOROMIR', race='human', alignment='G', stats=stats, new=False)
+# print(me)
+
+char = {
+    'name': 'RETER',
+    'characteristics': {
+        'alignment': 1,
+        'race': 3,
+        'hp': 50,
+        'max_hp': 60,
+        'xp': 9001,
+        'level': 1,
+        'vocation': 6,
+    },
+}
+me = Adventurer(**char)
 print(me)
-me = Adventurer(name='GIMLI', race='dwarf', new=True)
-print(me)
-me = Adventurer(name='BOROMIR', race='human', new=True)
-print(me)
+print(me.characteristics)
+me = Adventurer()
+print(me.characteristics)
