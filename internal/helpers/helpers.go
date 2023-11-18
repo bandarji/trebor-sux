63ccbe84380ed199b2087170231d0c72241baad9
Sean Jain Ellis <sean.ellis@upstart.com>
2023-11-17 14:12:45 -1000

    Draw a box added

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/bandarji/treborsux/internal/ansi"
 	c "github.com/bandarji/treborsux/internal/ansi"
 	"github.com/bandarji/treborsux/internal/types"
 )
@@ -17,4 +18,42 @@ func Splash() string {
 		sb.WriteString(c.Red(line))
 	}
 	return sb.String()
+}
+
+type Box struct {
+	Y, X, W, H     int
+	TL, TR, BL, BR rune
+	T, B, L, R     rune
+}
+
+func (b *Box) Init(y, x, w, h int, thick bool) {
+	b.Y, b.X, b.W, b.H = y, x, w, h
+	if thick {
+		b.TL, b.TR, b.BL, b.BR, b.T, b.B, b.L, b.R = '╔', '╗', '╚', '╝', '═', '═', '║', '║'
+	} else {
+		b.TL, b.TR, b.BL, b.BR, b.T, b.B, b.L, b.R = '┌', '┐', '└', '┘', '─', '─', '│', '│'
+	}
+}
+
+func (b Box) Draw() {
+	sb := strings.Builder{}
+	// Corners
+	sb.WriteString(fmt.Sprintf("%s%s", ansi.Pos(b.Y, b.X), ansi.Green(string(b.TL))))
+	sb.WriteString(fmt.Sprintf("%s%s", ansi.Pos(b.Y, b.X+b.W), ansi.Green(string(b.TR))))
+	sb.WriteString(fmt.Sprintf("%s%s", ansi.Pos(b.Y+b.H, b.X), ansi.Green(string(b.BL))))
+	sb.WriteString(fmt.Sprintf("%s%s", ansi.Pos(b.Y+b.H, b.X+b.W), ansi.Green(string(b.BR))))
+	// Top, bottom
+	sbSub := strings.Builder{}
+	for i := 0; i < b.W-1; i++ {
+		sbSub.WriteRune(b.T)
+	}
+	sb.WriteString(fmt.Sprintf("%s%s", ansi.Pos(b.Y, b.X+1), ansi.Green(sbSub.String())))
+	sb.WriteString(fmt.Sprintf("%s%s", ansi.Pos(b.Y+b.H, b.X+1), ansi.Green(sbSub.String())))
+	// Sides
+	for i := 1; i < b.H; i++ {
+		sb.WriteString(fmt.Sprintf("%s%s", ansi.Pos(b.Y+i, b.X), ansi.Green(string(b.L))))
+		sb.WriteString(fmt.Sprintf("%s%s", ansi.Pos(b.Y+i, b.X+b.W), ansi.Green(string(b.L))))
+	}
+	// Display
+	fmt.Print(sb.String())
 }
