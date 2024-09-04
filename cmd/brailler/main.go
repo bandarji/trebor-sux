32b4aa87b4fb898b71e651be3590b3ee2bdcdaf1
Sean Jain Ellis <sellis@bandarji.com>
2024-09-03 19:15:52 -1000

    Splash screen and monster images added

--- cmd/brailler/main.go
+++ cmd/brailler/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "time"
+import (
+	"image"
+	"log"
+	"os"
+)
+
+type Image struct {
+	ImageBytes image.Image
+	Braille    string
+}
+
+func (i *Image) Read() {
+	if len(os.Args) < 2 {
+		log.Fatal("no image file provided")
+	}
+	if fh, err := os.Open(os.Args[1]); err != nil {
+		log.Fatal(err)
+	} else {
+		defer fh.Close()
+		i.ImageBytes = DecodeImage(fh)
+	}
+}
+
+func DecodeImage(fh *os.File) (img image.Image) {
+	img, _, err := image.Decode(fh)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
 
 func main() {
-	time.Sleep(1 * time.Second)
+	asset := &Image{}
+	_ = asset
 }
